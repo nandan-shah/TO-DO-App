48578fa79e91f109e112f1bb876a67a1061edf66
nandan-shah <nandan2583@gmail.com>
2021-06-15 02:04:51 +0530

    ejs template added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+var workList=[];
 var items=["learn Node","learn express","make To-Do app"];
 
 app.get("/", (req, res) => {
@@ -15,14 +16,24 @@ app.get("/", (req, res) => {
     day: "numeric" }; 
 var todays = new Date();
 var today = todays.toLocaleDateString('en-US',options);
-res.render("list", { todayDay : today, newItem:items});
+res.render("list", { listTitle : today, newItem:items});
 });
 
-app.post("/", (req, res) => {
-    var newItem=req.body.next_item;
-    items.push(newItem);
-    res.redirect('/');
+app.get("/work", (req, res) => {
+    res.render("list", { listTitle :"Work", newItem:workList});
 })
+app.post("/", (req, res) => {
+    var Item=req.body.next_item;
+    if(req.body.listType==="Work"){
+        workList.push(Item);
+        res.redirect('/work');
+    }
+    else{
+        items.push(Item);
+        res.redirect('/');
+    }   
+})
+
 app.listen(3000, () => {
 console.log("runing on port 3000");
 });
